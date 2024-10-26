39061d47397e356779ddca19e95c0bd9e6d00565
김반석 <dalllo9851@gmail.com>
2024-10-26 13:51:47 +0900

    조회수 개별표시

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <h1>영화정보</h1>
-  <div>
-    <h3 class="bg-yellow" :style="textRed">{{ title }}</h3>
-    <p>개봉: {{ year }}</p>
-    <p>장르: {{ category }}</p>
+  <div v-for="(data, i) in data" :key="i">
+    <h3 class="bg-yellow" :style="textRed">{{ data.title }}</h3>
+    <p>개봉: {{ data.year }}</p>
+    <p>장르: {{ data.category }}</p>
+    <button @click="increseLike(i)">좋아요</button> <span>{{ data.cnt }}</span>
   </div>
-  <p v-for="(item,i) in foods" :key="i">
-    {{ item}}
-  </p>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
-      foods: ["김밥","순대","만두"],
-      title: "노량",
-      year: 2023,
-      category: "액션, 드라마",
-      textRed: "color: red",
-    }
-  }
-}
+      data: [
+        {
+          title: "노량",
+          year: 2023,
+          category: "액션, 드라마",
+          textRed: "color: red",
+          cnt: 0,
+        },
+        {
+          title: "아쿠아맨과 로스트 킹덤",
+          year: 2023,
+          category: "액션, 판타지, 어드벤처",
+          cnt: 0,
+        },
+        {
+          title: "3일의 휴가",
+          year: 2023,
+          category: "판타지, 드라마",
+          cnt: 0,
+        },
+      ],
+    };
+  },
+  methods: {
+    increseLike(i) {
+      this.data[i].cnt += 1;
+    },
+  },
+};
 </script>
 
 <style>
